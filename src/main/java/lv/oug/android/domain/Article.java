043cd369b2c4111dc0aa6f44ce73db14364b4423
echoAlexey <aleksejs.nikolajenko@gmail.com>
2014-02-09 19:30:32 +0200

    Article/News adapter for list view

---
+++ src/main/java/lv/oug/android/domain/Article.java
@@ -0,0 +1,102 @@
+package lv.oug.android.domain;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import com.j256.ormlite.field.DatabaseField;
+
+import java.util.Date;
+
+public class Article implements Parcelable {
+
+    @DatabaseField
+    private long id;
+
+    @DatabaseField
+    private String title;
+
+    @DatabaseField
+    private String description;
+
+    @DatabaseField
+    private Date createdAt;
+
+    @DatabaseField
+    private Date updatedAt;
+
+    public Article() {
+    }
+
+    private Article(Parcel in) {
+        id = in.readLong();
+        title = in.readString();
+        description = in.readString();
+        createdAt = new Date(in.readLong());
+        updatedAt = new Date(in.readLong());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeLong(id);
+        out.writeString(title);
+        out.writeString(description);
+        out.writeLong(createdAt.getTime());
+        out.writeLong(updatedAt.getTime());
+    }
+
+    public static final Parcelable.Creator<Article> CREATOR = new Parcelable.Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel in) {
+            return new Article(in);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[size];
+        }
+    };
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
--- src/main/java/lv/oug/android/presentation/news/NewsFragment.java
+++ src/main/java/lv/oug/android/presentation/news/NewsFragment.java
@@ -1,12 +1,27 @@
 package lv.oug.android.presentation.news;
 
+import android.os.Bundle;
+import android.widget.ListView;
+import butterknife.InjectView;
 import lv.oug.android.R;
+import lv.oug.android.domain.Article;
 import lv.oug.android.presentation.BaseFragment;
 
+import java.util.List;
+
 public class NewsFragment extends BaseFragment {
+
+    @InjectView(R.id.list_articles)
+    ListView listArticles;
 
     @Override
     protected int contentViewId() {
         return R.layout.news;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+        List<Article> articles = null;
+        listArticles.setAdapter(new ArticleListAdapter(this.getActivity(), R.layout.row_news_entity, articles));
     }
 }
